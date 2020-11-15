e09a06e6c9880c50f5af5e61352efac417d1a733
kkatoch89 <karankatoch.hba@gmail.com>
2020-11-15 15:30:00 -0500

    Added functionality so that burger dynamically renders based on changes to ingredients state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,29 @@ import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+	// Object.keys() creates an array of strings of keys.
+	// Array(3) would create an array with 3 undefined spots.
+	// reduce is used to flatten the array from an array of arrays filled with objects
+	// to an array of objects
+	let transformedIngredients = Object.keys(props.ingredients)
+		.map((igKey) => {
+			return [...Array(props.ingredients[igKey])].map((_, i) => {
+				return <BurgerIngredient key={igKey + i} type={igKey} />;
+			});
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el);
+		}, []);
+
+	if (transformedIngredients.length === 0) {
+		transformedIngredients = <p>Please start adding ingredients!</p>;
+	}
+	console.log(transformedIngredients);
+
 	return (
 		<div className={styles.Burger}>
 			<BurgerIngredient type="bread-top" />
-			<BurgerIngredient type="cheese" />
-			<BurgerIngredient type="meat" />
+			{transformedIngredients}
 			<BurgerIngredient type="bread-bottom" />
 		</div>
 	);
