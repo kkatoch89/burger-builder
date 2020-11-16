41efc723959adf8f8aa72c7fd540892b8e15ac0b
kkatoch89 <karankatoch.hba@gmail.com>
2020-11-16 17:37:08 -0500

    Created navigation components so that can be plugged in and used by both the mobile nav (side-drawer) and top fixed nav

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
 import styles from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => {
 	return (
 		<>
-			<div>Toolbar, SideDrawer, Backdrop</div>
+			<Toolbar />
+			<SideDrawer />
 			<main className={styles.Content}>{props.children}</main>
 		</>
 	);
