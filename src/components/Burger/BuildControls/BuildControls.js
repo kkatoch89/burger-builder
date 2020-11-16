a8360eaf46eef34f0a8a248a67c7d006a9709b3f
kkatoch89 <karankatoch.hba@gmail.com>
2020-11-16 12:49:47 -0500

    Added order button with functionality so that it's disabled when nothing has been added to burger

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -27,6 +27,9 @@ const buildControls = (props) => {
 					/>
 				);
 			})}
+			<button className={styles.OrderButton} disabled={!props.purchasable}>
+				ORDER NOW
+			</button>
 		</div>
 	);
 };
