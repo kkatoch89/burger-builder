3c3d241350180b1484a9847329c3bce978b03dcf
kkatoch89 <karankatoch.hba@gmail.com>
2020-11-16 18:13:13 -0500

    Made navigation & logo responsive, hid desktop nav in mobile view also. Starting to add functionality to sidedrawer, so far able to slide open sidedrawer; burger menu icon not added yet

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => {
-	return (
-		<>
-			<Toolbar />
-			<SideDrawer />
-			<main className={styles.Content}>{props.children}</main>
-		</>
-	);
-};
+class Layout extends Component {
+	state = {
+		showSideDrawer: true,
+	};
 
-export default layout;
+	sideDrawerClosedHandler = () => {
+		this.setState({ showSideDrawer: false });
+	};
+
+	render() {
+		return (
+			<>
+				<Toolbar />
+				<SideDrawer
+					closed={this.sideDrawerClosedHandler}
+					open={this.state.showSideDrawer}
+				/>
+				<main className={styles.Content}>{this.props.children}</main>
+			</>
+		);
+	}
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,17 +3,26 @@ import React from 'react';
 import styles from './SideDrawer.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
+	let attachedClasses = [styles.SideDrawer, styles.Close];
+	if (props.open) {
+		attachedClasses = [styles.SideDrawer, styles.Open];
+	}
+
 	return (
-		<div className={styles.SideDrawer}>
-			<div className={styles.Logo}>
-				<Logo />
+		<>
+			<Backdrop show={props.open} clicked={props.closed} />
+			<div className={attachedClasses.join(' ')}>
+				<div className={styles.Logo}>
+					<Logo />
+				</div>
+				<nav className={styles.DesktopOnly}>
+					<NavigationItems />
+				</nav>
 			</div>
-			<nav>
-				<NavigationItems />
-			</nav>
-		</div>
+		</>
 	);
 };
 
